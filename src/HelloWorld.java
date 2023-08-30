e933ad18b0aa6db57505f3811be32d7c0c58413d
30195903 <30195903@nescol.ac.uk>
2023-08-30 11:32:00 +0100

    Will add code that gives

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * My First Program
+ * @author Otormiefe Dafinone
+ * @version 1
+ */
+    public class HelloWorld {
+        // Write out Hello World to the console
+        public static void main (String[] args) {
+            System.out.println("Hello World");
+        }
+    }
+
---
+++ src/Lesson_1.java
@@ -0,0 +1,3 @@
+public class Lesson_1 {
+    public static void main (string[] args) {System.out.println("Hello World")}
+}
