2bfa9f8e1f9283bff9f9607a056822d7381225f9
30195903 <30195903@nescol.ac.uk>
2023-08-30 12:18:28 +0100

    will add a Haiku for Excercise 1

--- src/Lesson_1.java
+++ src/Lesson_1.java
@@ -1,3 +1,9 @@
 public class Lesson_1 {
-    public static void main (string[] args) {System.out.println("Hello World")}
+    public static void main (String[] args) {
+        System.out.println(
+                "Heat wave scorches land" +
+                        "Crops wilt under blazing sun\n" +
+                        "Climate change marches on"
+
+        );}
 }
