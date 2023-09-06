72a42c87c799e1130faf6daff82b5cd7ecf57022
30195903 <30195903@nescol.ac.uk>
2023-09-06 10:34:01 +0100

    Will add exercise 1 to the repository, I did this on 06/09/2023

---
+++ src/Lesson2_Excercise_1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Lesson2_Excercise_1 {
+    public static void main(String[] args) {
+        Scanner character = new Scanner(System.in);
+        System.out.println("What is the name of your main character?");
+        String chara = character.nextLine();
+
+        Scanner hobby = new Scanner(System.in);
+        System.out.println("What is your hobby?");
+        String hob = hobby.nextLine();
+
+        System.out.println(chara +" knew that he had to get better at " + hob + "\n" +
+                "he wanted to succeed. He had been spending every waking moment of\n" +
+                "his free time practicing and perfecting his craft.");
+    }
+}
