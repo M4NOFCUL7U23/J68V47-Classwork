bb587e7c49f6b34bf33badfd156cd0002288679c
30195903 <yolodino234@gmail.com>
2023-09-13 11:36:45 +0100

    Will Add Lesson 3 Exercise 2 to the repository

---
+++ src/L3E2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class L3E2 {
+    public static void main(String[] args) {
+        Scanner Question1 = new Scanner(System.in);
+        System.out.println("What is the capital of Nigeria?");
+        String Q1 = Question1.nextLine();
+
+        Scanner Question2 = new Scanner(System.in);
+        System.out.println("What is the capital of the Uk?");
+        String Q2 = Question2.nextLine();
+
+        Scanner Question3 = new Scanner(System.in);
+        System.out.println("What is the capital of Italy?");
+    }
+}
