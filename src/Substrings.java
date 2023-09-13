795080d36c6327cbbf6ce5750606813db4e8cd13
30195903 <yolodino234@gmail.com>
2023-09-13 09:41:35 +0100

    Will give examples on how to use substrings

---
+++ src/Substrings.java
@@ -0,0 +1,10 @@
+public class Substrings {
+    public static void main(String[] args) {
+        //Substrings
+        String message = "Hello world";
+        System.out.println(message.substring(0,2)); //Displays "e"
+        System.out.println(message.substring(3,8)); //displays "o wo"
+        System.out.println(message.substring(0,2)); //displays "H"
+
+    }
+}
