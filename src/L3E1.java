65d13df4e7d877e16cde11a58b3eaf4e89bb6e2b
30195903 <yolodino234@gmail.com>
2023-09-13 10:07:12 +0100

    Will add Lesson 3 exercise 1 to the repository.

---
+++ src/L3E1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner
+public class L3E1 {
+    public static void main(String[] args) {
+        Scanner firstname = new Scanner(System.in);
+        System.out.println("Enter your first name: ");
+
+        Scanner surname = new Scanner(System.in);
+        System.out.println("Enter your surname: ");
+
+        Scanner DOB = new Scanner(System.in);
+        System.out.println("Enter your year of birth YYYY");
+
+
+    }
+}
