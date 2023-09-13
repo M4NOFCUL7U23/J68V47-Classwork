0d5c2a4da6c045f4f56f3ac9b1e3a4b44bc3e9f1
30195903 <yolodino234@gmail.com>
2023-09-13 09:44:11 +0100

    Will give examples on how to use to change strings to upper case and lower case

---
+++ src/Changing_case.java
@@ -0,0 +1,8 @@
+public class Changing_case {
+    public static void main(String[] args) {
+        String message = "Hello World";
+        System.out.println(message.toLowerCase()); //hello world
+        System.out.println(message.toUpperCase()); //HELLO WORLD
+
+    }
+}
