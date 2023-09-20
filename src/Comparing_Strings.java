6519c4bfa33101fab81d34abd52a4842dc81e36c
30195903 <30195903@nescol.ac.uk>
2023-09-20 11:07:08 +0100

    will be adding examples for comparing Strings

---
+++ src/Comparing_Strings.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Comparing_Strings {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Would you like express shipping?: ");
+        String answer = input.nextLine();
+        if (answer == "yes"); {
+            System.out.println("That will be an extra £10");
+        }
+        System.out.println("Have a nice day");
+    }
+
+}
