1f6b218addeba05979fb2d50db43d8cd0cf7c19e
30195903 <30195903@nescol.ac.uk>
2023-09-20 09:39:10 +0100

    will give an example on how to use if statements

---
+++ src/IF_statements.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class IF_statements {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is the answer to the sum 3 + 4? ");
+        int answer = input.nextInt();
+        if (answer == 7) {
+            System.out.println("Well done that is correct");
+
+        }
+
+    }
+}
---
+++ src/L4E1.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class L4E1 {
+    public static void main(String[] args) {
+
+        }
+
+    }
