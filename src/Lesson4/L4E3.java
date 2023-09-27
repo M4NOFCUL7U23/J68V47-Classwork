6bf4c75383973f937669990b4892d93dc7864ec5
30195903 <30195903@nescol.ac.uk>
2023-09-27 09:37:54 +0100

    Attempted to do Lesson 4 Exercise 3

---
+++ src/Lesson4/L4E3.java
@@ -0,0 +1,26 @@
+package Lesson4;
+import java.util.Scanner;
+
+public class L4E3 {
+    public static void main(String[] args) {
+        Scanner LoanAmount = new Scanner(System.in);
+        System.out.print("Loan amount: ");
+        int L = LoanAmount.nextInt();
+
+        Scanner InterestRate = new Scanner(System.in);
+        System.out.print("Interest Rate (APR %): ");
+        int j = InterestRate.nextInt();
+
+        Scanner NumberOfYears = new Scanner(System.in);
+        System.out.print("Number of years: ");
+        int n = NumberOfYears.nextInt();
+
+        Scanner MonthlyPayment = new Scanner(System.in);
+        System.out.print("Monthly payment for this loan: ");
+        int M = MonthlyPayment.nextInt();
+
+        M = L(j / (1 - (1 + j)^-n));
+
+
+    }
+}
