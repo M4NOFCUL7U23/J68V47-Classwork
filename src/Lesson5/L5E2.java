396d6728a58483bfda3553d00672396e3edc5969
30195903 <30195903@nescol.ac.uk>
2023-09-27 10:03:52 +0100

    Completed Lesson 5 exercise 2

---
+++ src/Lesson5/L5E2.java
@@ -0,0 +1,13 @@
+package Lesson5;
+
+public class L5E2 {
+    public static void main(String[] args) {
+        for (int count=1; count<=10; count++){
+
+            System.out.println(count);
+        }
+        System.out.print("Blastoff !!!!!!");
+        }
+
+    }
+
