15c170e88b83687c2c0fa075a8df4fd5ab859856
ManOfCulture <yolodino234@gmail.com>
2024-01-06 02:42:50 +0000

    Catcting up on missed class work, starting with lesson 6 exercise 3

--- src/Lesson6/L6E3.java
+++ src/Lesson6/L6E3.java
@@ -1,4 +1,12 @@
 package Lesson6;
+import java.util.*;
 
 public class L6E3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int score = 0;
+        int lives = 3;
+        int userAnswer;
+    }
 }
